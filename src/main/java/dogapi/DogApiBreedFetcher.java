941ea15e97ef87c43b87b5cc9d488d6056bcc573
elainexing <elainexing99@gmail.com>
2025-10-20 12:42:54 -0400

    implemented Task 3 and 4, and updated exceptions for all files

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -26,7 +26,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // return statement included so that the starter code can compile and run.
         client.newBuilder()
                 .build();
@@ -50,7 +50,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 throw new BreedNotFoundException(breed);
             }
         }
-        catch (IOException | RuntimeException event) {
+        catch (Exception event) {
             throw new BreedNotFoundException(breed);
         }
 
